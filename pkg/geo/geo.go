b1e95558142ad3e59b5f633772fb987c000a0ccc
Joao Henrique Machado Silva <joaoh82@gmail.com>
2017-02-23 08:36:32 -0300

    Refactoring to make the code more readable

--- pkg/geo/geo.go
+++ pkg/geo/geo.go
@@ -22,23 +22,24 @@ func round(val float64, roundOn float64, places int) (newVal float64) {
 	return
 }
 
-// Distance function returns the distance (in meters) between two points of
-// a given longitude and latitude relatively accurately (using a spherical
-// approximation of the Earth) through the Haversin Distance Formula for
-// great arc distance on a pshere with accuracy for small distances
+// Distance function returns the distance (in meters) between two points on
+// earth of a given longitude and latitude relatively
+// through the Haversin Distance Formula for great arc distance on a sphere with
+// accuracy for small distances.
 // Point coordinates are supplied in degrees and converted into radius.
-func Distance(lat1, lon1, lat2, lon2 float64) float64 {
+func Distance(latOrigin, lonOrigin, latDest, lonDest float64) float64 {
 	// Converting to radians
 	var la1, lo1, la2, lo2, r float64
-	la1 = lat1 * math.Pi / 180
-	lo1 = lon1 * math.Pi / 180
-	la2 = lat2 * math.Pi / 180
-	lo2 = lon2 * math.Pi / 180
+	la1 = latOrigin * math.Pi / 180
+	lo1 = lonOrigin * math.Pi / 180
+	la2 = latDest * math.Pi / 180
+	lo2 = lonDest * math.Pi / 180
 
 	r = 6373000 // Earth radius in Meters
 
 	// Calculating
 	h := haversin(la2-la1) + math.Cos(la1)*math.Cos(la2)*haversin(lo2-lo1)
 
+	// Rounding and returning the result
 	return round(2*r*math.Asin(math.Sqrt(h)), .5, 0)
 }
